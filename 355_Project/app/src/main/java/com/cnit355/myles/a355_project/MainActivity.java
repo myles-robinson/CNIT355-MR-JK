9658daaaeb78f135172af0723d5e5f7890da8445
jkwak <kwakjonathan7@gmail.com>
2017-11-07 21:55:49 -0500

    added writing capabilities to firebase

--- 355_Project/app/src/main/java/com/cnit355/myles/a355_project/MainActivity.java
+++ 355_Project/app/src/main/java/com/cnit355/myles/a355_project/MainActivity.java
@@ -3,14 +3,26 @@ package com.cnit355.myles.a355_project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabase;
+    private String mUserId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +30,57 @@ public class MainActivity extends AppCompatActivity {
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        if (mFirebaseUser == null) {
+            // Not logged in, launch the Log In activity
+            loadLogInView();
+        } else {
+            mUserId = mFirebaseUser.getUid();
+
+            // Set up ListView
+            final ListView listView = (ListView) findViewById(R.id.eventListView);
+            final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+            listView.setAdapter(adapter);
+
+            // Add items via the Button and EditText at the bottom of the view.
+            final EditText text = (EditText) findViewById(R.id.addEventEditText);
+            final Button button = (Button) findViewById(R.id.addButton);
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    mDatabase.child("users").child(mUserId).child("items").push().child("title").setValue(text.getText().toString());
+                    text.setText("");
+                }
+            });
+
+            // Use Firebase to populate the list.
+            mDatabase.child("users").child(mUserId).child("items").addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    adapter.add((String) dataSnapshot.child("title").getValue());
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    adapter.remove((String) dataSnapshot.child("title").getValue());
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
 
         if (mFirebaseUser == null) {
             // Not logged in, launch the Log In activity
